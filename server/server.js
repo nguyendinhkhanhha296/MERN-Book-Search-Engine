19950ddb2ab975af4579876b0a49739eb0480425
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-11-08 23:19:45 -0800

    fix auth.js and create schemas folder to involve resolvers and typeDefs

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const routes = require('./routes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -23,9 +25,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+app.use(routes);
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
